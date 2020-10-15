ed89b62270ddcd7644001b6f50113fdff5a04225
Angle <Ribbonwing@gmail.com>
2020-10-14 19:36:16 -0600

    use ArrayList in World instead of array

--- src/net/angle/omnicraft/world/World.java
+++ src/net/angle/omnicraft/world/World.java
@@ -6,6 +6,8 @@
 package net.angle.omnicraft.world;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import net.angle.omnicraft.pixel.GreyVariedPixelSource;
 import net.angle.omnicraft.random.OmniRandom;
 import net.angle.omnicraft.world.blocks.Block;
@@ -22,22 +24,22 @@ import net.angle.omnicraft.world.types.SoilType;
  * @author angle
  */
 public class World {
-    private Block[] blocks;
+    private List<Block> blocks;
     private Chunk chunk;
     
     public World() {
-            blocks = new Block[2];
-            blocks[0] = new SoilBlock(new SoilType(new SoilFraction(new MineralGrain(
+            blocks = new ArrayList<>();
+            blocks.add(new SoilBlock(new SoilType(new SoilFraction(new MineralGrain(
                     new Mineraloid(new GreyVariedPixelSource(Color.darkGray, 60)), 1.0f), 100.0f)),
-                new SteppedCubeShape(12),new OmniRandom());
+                new SteppedCubeShape(12),new OmniRandom()));
             
-            chunk = new OctreeChunk(blocks[0]);
+            chunk = new OctreeChunk(blocks.get(0));
             
-            blocks[1] = new SoilBlock(new SoilType(new SoilFraction(new MineralGrain(
+            blocks.add(new SoilBlock(new SoilType(new SoilFraction(new MineralGrain(
                     new Mineraloid(new GreyVariedPixelSource(Color.darkGray, 60)), 1.0f), 100.0f)),
-                new CubeShape(),new OmniRandom());
+                new CubeShape(),new OmniRandom()));
             
-            chunk.setBlock(0, 0, 0, blocks[1]);
+            chunk.setBlock(0, 0, 0, blocks.get(1));
         
     }
     
