ba97e1ccbfa651f95a10cdb33ba7f07885049065
Angle <Ribbonwing@gmail.com>
2020-10-23 10:30:38 -0600

    moved methods to TextureSource and made them default

--- src/net/angle/omnicraft/textures/AbstractTextureSource.java
+++ src/net/angle/omnicraft/textures/AbstractTextureSource.java
@@ -8,18 +8,9 @@ package net.angle.omnicraft.textures;
 import com.samrj.devil.gl.DGL;
 import com.samrj.devil.gl.Image;
 import com.samrj.devil.gl.Texture2D;
-import com.samrj.devil.math.Util;
 import java.awt.Color;
 import net.angle.omnicraft.random.OmniRandom;
 import net.angle.omnicraft.textures.pixels.PixelSource;
-import static org.lwjgl.opengl.ARBTextureFilterAnisotropic.GL_TEXTURE_MAX_ANISOTROPY;
-import static org.lwjgl.opengl.GL11.GL_LINEAR;
-import static org.lwjgl.opengl.GL11.GL_NEAREST;
-import static org.lwjgl.opengl.GL11.GL_REPEAT;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_S;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_T;
 
 /**
  *
@@ -37,41 +28,6 @@ public abstract class AbstractTextureSource implements TextureSource {
     }
     
     public abstract Color[][] getPixelColors(int width, int height, OmniRandom random);
-    
-    public Image generateImage(int width, int height, Color[][] tex) {
-        Image image = DGL.genImage(width, height, 3, Util.PrimType.BYTE);
-        image.shade((x, y, band) -> {
-            if (band == 0)
-                return tex[x][y].getRed();
-            if (band == 1)
-                return tex[x][y].getGreen();
-            if (band == 2)
-                return tex[x][y].getBlue();
-            else
-                System.out.println("Asked for Band: " + band);
-                return 0;
-        });
-        
-        return image;
-    }
-    
-    public void configureTexture(Image image, Texture2D texture) {
-        texture.bind();
-        texture.parami(GL_TEXTURE_WRAP_S, GL_REPEAT);
-        texture.parami(GL_TEXTURE_WRAP_T, GL_REPEAT);
-        texture.parami(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        texture.parami(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        texture.image(image);
-        DGL.delete(image);
-        texture.generateMipmap();
-        texture.paramf(GL_TEXTURE_MAX_ANISOTROPY, 8);
-        texture.unbind();
-    }
-
-    @Override
-    public Texture2D generateTexture(OmniRandom random) {
-        return generateTexture(16, 16, random);
-    }
     
     @Override
     public Texture2D generateTexture(int width, int height, OmniRandom random) {
