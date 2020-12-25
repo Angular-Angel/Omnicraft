41d44dd792ef4d1eedda8f16181621f7a6fd1f72
Angle <Ribbonwing@gmail.com>
2020-12-25 13:59:34 -0700

    add try/catch loop and avoid debug leak spam on crashes

--- src/net/angle/omnicraft/client/DebugClient.java
+++ src/net/angle/omnicraft/client/DebugClient.java
@@ -14,6 +14,7 @@ import net.angle.omnicraft.world.World;
 import net.angle.omnicraft.world.WorldGenerator;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.system.APIUtil;
 import org.lwjgl.system.MemoryStack;
 
 /**
@@ -28,62 +29,73 @@ public class DebugClient {
     private World world;
     
     public void run() {
-        Game.setTitle("Omnicraft");
-        Game.setDebug(true);
-        
-        Game.onInit(() -> {
-            DGL.init();
- 
-            camera = new Camera3D(0.1f, 100.0f, Util.toRadians(90.0f), 1.0f);
-            
-            Vec2i resolution = Game.getResolution();
-            camera.setFOV(resolution.x, resolution.y, Util.toRadians(90.0f));
-            
-            player = new Player(camera);
-            
-            world = WorldGenerator.generateWorld();
-            
-            glEnable(GL_TEXTURE_2D);
-            
-            glShadeModel(GL_SMOOTH);                        // Enables Smooth Shading
-            glClearDepth(1.0f);                         // Depth Buffer Setup
-            glEnable(GL_DEPTH_TEST);                        // Enables Depth Testing
-            glDepthFunc(GL_LEQUAL);                         // The Type Of Depth Test To Do
-            glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);          // Really Nice Perspective Calculations*/
-            
-            Game.getMouse().setGrabbed(true);
-            Game.setVsync(true);
-        });
-        
-        Game.onStep((float dt) -> {
-            player.update(dt);
-            if (Game.getKeyboard().isKeyDown(GLFW_KEY_ESCAPE)){
-                Game.stop();
-            }
-        });
-        
-        Game.onRender(() -> {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        try
+        {
+            Game.setTitle("Omnicraft");
+            Game.setDebug(true);
 
-            //I need to add matrix toArray functions to make this easier. -Sam
-            try (MemoryStack stack = MemoryStack.stackPush())
-            {
-                glMatrixMode(GL_PROJECTION);
-                glLoadMatrixf(camera.projMat.mallocFloat(stack));
-                glMatrixMode(GL_MODELVIEW);
-                glLoadMatrixf(camera.viewMat.mallocFloat(stack));
-            }
-            
-            world.draw();
-        });
-        
-        Game.onDestroy(crashed ->
-            {
-                world.delete();
-                DGL.destroy();
+            Game.onInit(() -> {
+                DGL.init();
+
+                camera = new Camera3D(0.1f, 100.0f, Util.toRadians(90.0f), 1.0f);
+
+                Vec2i resolution = Game.getResolution();
+                camera.setFOV(resolution.x, resolution.y, Util.toRadians(90.0f));
+
+                player = new Player(camera);
+
+                world = WorldGenerator.generateWorld();
+
+                glEnable(GL_TEXTURE_2D);
+
+                glShadeModel(GL_SMOOTH);                        // Enables Smooth Shading
+                glClearDepth(1.0f);                         // Depth Buffer Setup
+                glEnable(GL_DEPTH_TEST);                        // Enables Depth Testing
+                glDepthFunc(GL_LEQUAL);                         // The Type Of Depth Test To Do
+                glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);          // Really Nice Perspective Calculations*/
+
+                Game.getMouse().setGrabbed(true);
+                Game.setVsync(true);
             });
-        
-        Game.run();
+
+            Game.onStep((float dt) -> {
+                player.update(dt);
+                if (Game.getKeyboard().isKeyDown(GLFW_KEY_ESCAPE)){
+                    Game.stop();
+                }
+            });
+
+            Game.onRender(() -> {
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+                //I need to add matrix toArray functions to make this easier. -Sam
+                try (MemoryStack stack = MemoryStack.stackPush())
+                {
+                    glMatrixMode(GL_PROJECTION);
+                    glLoadMatrixf(camera.projMat.mallocFloat(stack));
+                    glMatrixMode(GL_MODELVIEW);
+                    glLoadMatrixf(camera.viewMat.mallocFloat(stack));
+                }
+
+                world.draw();
+            });
+
+            Game.onDestroy(crashed ->
+                {
+                    world.delete();
+                    world = null;
+                    
+                    if (crashed) DGL.setDebugLeakTracking(false);
+                    
+                    DGL.destroy();
+                });
+
+            Game.run();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            APIUtil.DEBUG_STREAM.close(); //Prevent LWJGL leak message spam.
+            System.exit(-1);
+        }
     }
     
     public static void main(String args[]) {
